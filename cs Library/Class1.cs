50872c11dacda668151709fe8cd6a57b1f868f3b
900 <mao090001@gmail.com>
2024-05-15 18:45:43 +0300

    Added C# code with a class lib for creating and dealing with bitboards. These will be compiled and included as dynamic libs. Will may use for different purposes later.

---
+++ cs Library/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace cs_Library;
+
+public class Bitboards
+{
+    // Bitboards will live and delt with here.
+    public static int bitboard_generator(char piece_to_search, string board){
+
+        // Necesarry checks for valid inputs into function
+        if (!"KkQqRrBbNnPp".Contains(piece_to_search)){
+            Console.WriteLine("There is a problem with the piece we are searching on the board! - cs_Library>Bitboards>bitboard_generator");
+        }
+
+        int bitboard = 0;
+        for (int i = 0; i < board.Length; i++){
+            if (char.IsDigit(board[i])){
+                i += (int)board[i] - 1;
+            }
+
+            if (board[i] == piece_to_search){
+                bitboard |= 1 << i;
+            }
+        }
+        return bitboard;
+    }
+}
---
+++ cs Library/README.txt
@@ -0,0 +1,2 @@
+This file called "cs Library" is to include all components and functionality of the program writen in C#
+Will be compiled into a .dll and used in rest of the codebase.
